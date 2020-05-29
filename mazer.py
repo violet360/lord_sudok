b8f003607cf9b5c01389b7677b908746c035f46f
violet360 <shashankkarmakar2000@gmail.com>
2020-05-29 13:15:00 +0530

    k

--- mazer.py
+++ mazer.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import operator
+path = 'sudoku.jpg'
 
 def show_image(img):
 	cv2.imshow('image', img)
@@ -20,30 +21,19 @@ def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
 
 	for point in points:
 		img = cv2.circle(img, tuple(int(x) for x in point), radius, colour, -1)
-	show_image(img)
+	# show_image(img)
 	return img
 
-
-
-def plot_many_images(images, titles, rows=1, columns=2):
-	for i, image in enumerate(images):
-		plt.subplot(rows, columns, i+1)
-		plt.imshow(image, 'gray')
-		plt.title(titles[i])
-		plt.xticks([]), plt.yticks([])
-	plt.show()
 
 def pre_process_image(img, skip_dilate=False):
 	proc = cv2.GaussianBlur(img.copy(), (9, 9), 0)
 	proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+	# cv2.imshow('image', proc)
 	proc = cv2.bitwise_not(proc, proc)
 	kernal = np.ones((2,2), np.uint8)
-	# proc = cv2.erode(proc, kernal, iterations=1)
+	# # proc = cv2.erode(proc, kernal, iterations=1)
 	proc = cv2.dilate(proc, kernal, iterations = 1)
 	return proc
-
-img = cv2.imread('sudoku.jpg', cv2.IMREAD_GRAYSCALE)
-processed = pre_process_image(img)
 
 def find_corners_of_largest_polygon(img):
 	contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -78,12 +68,147 @@ def crop_and_warp(img, crop_rect):
 
 	m = cv2.getPerspectiveTransform(src, dst)
 	img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+	# show_image(m)
 	return cv2.warpPerspective(img, m, (int(side), int(side)))
 
 
-img = cv2.imread('sudoku.jpg', cv2.IMREAD_GRAYSCALE)
+def infer_grid(img):
+	"""Infers 81 cell grid from a square image."""
+	squares = []
+	side = img.shape[:1]
+	side = side[0] / 9
+	for i in range(9):
+		for j in range(9):
+			p1 = (i * side, j * side)  # Top left corner of a bounding box
+			p2 = ((i + 1) * side, (j + 1) * side)  # Bottom right corner of bounding box
+			squares.append((p1, p2))
+	return squares
+
+
+def display(in_img, rects, colour=255):
+	"""Displays rectangles on the image."""
+	img = in_img.copy()
+	for rect in rects:
+		img = cv2.rectangle(img, tuple(int(x) for x in rect[0]), tuple(int(x) for x in rect[1]), colour)
+	# show_image(img)
+	return img
+
+
+def slash_rect(img, rect):
+	return img[int(rect[0][1]):int(rect[1][1]), int(rect[0][0]):int(rect[1][0])]
+
+
+def scale_and_centre(img, size, margin=0, background=0):
+	h, w = img.shape[:2]
+
+	def centre_pad(length):
+		if length % 2 == 0:
+			side1 = int((size - length) / 2)
+			side2 = side1
+		else:
+			side1 = int((size - length) / 2)
+			side2 = side1 + 1
+		return side1, side2
+
+	def scale(r, x):
+		return int(r * x)
+
+	if h > w:
+		t_pad = int(margin / 2)
+		b_pad = t_pad
+		ratio = (size - margin) / h
+		w, h = scale(ratio, w), scale(ratio, h)
+		l_pad, r_pad = centre_pad(w)
+	else:
+		l_pad = int(margin / 2)
+		r_pad = l_pad
+		ratio = (size - margin) / w
+		w, h = scale(ratio, w), scale(ratio, h)
+		t_pad, b_pad = centre_pad(h)
+
+	img = cv2.resize(img, (w, h))
+	img = cv2.copyMakeBorder(img, t_pad, b_pad, l_pad, r_pad, cv2.BORDER_CONSTANT, None, background)
+	return cv2.resize(img, (size, size))
+
+
+def find_largest_feature(inp_img, scan_tl=None, scan_br=None):
+	img = inp_img.copy()
+	height, width = img.shape[:2]
+
+	max_area = 0
+	seed_point = (None, None)
+
+	if scan_tl is None:
+		scan_tl = [0, 0]
+
+	if scan_br is None:
+		scan_br = [width, height]
+
+	for x in range(scan_tl[0], scan_br[0]):
+		for y in range(scan_tl[1], scan_br[1]):
+			# Only operate on light or white squares
+			if img.item(y, x) == 255 and x < width and y < height:
+				area = cv2.floodFill(img, None, (x, y), 64)
+				if area[0] > max_area:
+					max_area = area[0]
+					seed_point = (x, y)
+
+	for x in range(width):
+		for y in range(height):
+			if img.item(y, x) == 255 and x < width and y < height:
+				cv2.floodFill(img, None, (x, y), 64)
+
+	mask = np.zeros((height + 2, width + 2), np.uint8)
+
+	# Highlight the main feature
+	if all([p is not None for p in seed_point]):
+		cv2.floodFill(img, mask, seed_point, 255)
+
+	top, bottom, left, right = height, 0, width, 0
+
+	for x in range(width):
+		for y in range(height):
+			if img.item(y, x) == 64:
+				cv2.floodFill(img, mask, (x, y), 0)
+
+			if img.item(y, x) == 255:
+				top = y if y < top else top
+				bottom = y if y > bottom else bottom
+				left = x if x < left else left
+				right = x if x > right else right
+
+	bbox = [[left, top], [right, bottom]]
+	return img, np.array(bbox, dtype='float32'), seed_point
+
+
+def extract_digit(img, rect, size):
+
+	digit = slash_rect(img, rect)
+
+	h, w = digit.shape[:2]
+	margin = int(np.mean([h, w]) / 2.5)
+	_, bbox, seed = find_largest_feature(digit, [margin, margin], [w - margin, h - margin])
+	digit = slash_rect(digit, bbox)
+
+	w = bbox[1][0] - bbox[0][0]
+	h = bbox[1][1] - bbox[0][1]
+
+	if w > 0 and h > 0 and (w * h) > 100 and len(digit) > 0:
+		return scale_and_centre(digit, size, 4)
+	else:
+		return np.zeros((size, size), np.uint8)
+
+def get_digits(img, squares, size):
+	digits = []
+	img = pre_process_image(img.copy(), skip_dilate=True)
+	for square in squares:
+		digits.append(extract_digit(img, square, size))
+	return digits
+
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 processed = pre_process_image(img)
 corners = find_corners_of_largest_polygon(processed)
-# show_image(processed)
-cropped = crop_and_warp('sudoku.jpg', corners)
-show_image(cropped)
+cropped = crop_and_warp(path, corners)
+squares = infer_grid(cropped)
+x  = display(cropped, squares)
+show_image(x)
